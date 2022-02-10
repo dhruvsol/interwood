247541dc13072145dcebe226d044139c93b5bfdf
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-02-10 16:22:04 +0530

    fix:tailwind

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar1 = () => {
       <Navbar className="bg-[#171717]" expand={false}>
         <Container fluid>
           <Navbar.Brand href="#">
-            <img src="/img/logo.svg" alt="logo" />
+            <img src="../../../img/logo.svg" alt="logo" />
           </Navbar.Brand>
           <Navbar.Toggle className="" aria-controls="offcanvasNavbar" />
           <Navbar.Offcanvas
